27f3827591cd86cbaa1ded2f964425a582b6a2b7
Willson0 <logvinovnikita3@gmail.com>
2026-02-09 02:44:47 +0500

    beta-test

--- backend/app/Http/Requests/OrderCreateRequest.php
+++ backend/app/Http/Requests/OrderCreateRequest.php
@@ -26,7 +26,7 @@ class OrderCreateRequest extends FormRequest
             "address.address" => "required|string",
             "address.latitude" => "required|numeric",
             "address.longitude" => "required|numeric",
-            "address.commentAddress" => "required|string",
+            "address.commentAddress" => "required|string|min:0",
 
             "paymentType" => "required|boolean",
         ];
